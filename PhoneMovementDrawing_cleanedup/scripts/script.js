cb7bac27467db2ba853d8c8121f7211c6ac4bc2a
Jess Herrington <35942642+Jessie12345@users.noreply.github.com>
2023-11-21 11:49:49 +1100

    Add files via upload

---
+++ PhoneMovementDrawing_cleanedup/scripts/script.js
@@ -0,0 +1,17 @@
+const NativeUI = require('NativeUI');
+const Patches = require('Patches');
+const Reactive = require('Reactive');
+const CameraInfo = require('CameraInfo');
+
+var slider = NativeUI.slider;
+
+if(Reactive.or(CameraInfo.isRecordingVideo, CameraInfo.isCapturingPhoto)){
+    slider.visible = false;
+}
+
+// //slider.configure(configuration);
+slider.visible = true;
+
+slider.value.monitor().subscribe(function(val) {
+    Patches.setScalarValue("sliderValue", val.newValue);
+});
